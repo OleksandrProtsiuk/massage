c03236293f82581ede3f855dd14f9206c1e4ae3f
alex <alex_is_ok@yahoo.com>
2019-04-23 00:31:17 +0300

    vue-bootstrap added

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,12 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
+
+import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+
+Vue.use(BootstrapVue);
