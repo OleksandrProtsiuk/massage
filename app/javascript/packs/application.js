f009e04e290b758f3b33fea2f9d152102f32a2c2
alex <alex_is_ok@yahoo.com>
2019-04-23 16:25:08 +0300

    navbar added

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,12 +3,25 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start();
-require("turbolinks").start();
+/* require("@rails/ujs").start() */
+/* require("turbolinks").start() */
 require("@rails/activestorage").start();
 require("channels");
 
-import Vue from 'vue'
-import BootstrapVue from 'bootstrap-vue'
+import Vue from 'vue/dist/vue.esm.js';
+import BootstrapVue from 'bootstrap-vue';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 Vue.use(BootstrapVue);
+
+new Vue({
+    el: '#app',
+    data: {
+        name: 'VALERA'
+    },
+    computed: {
+
+    }
+});
