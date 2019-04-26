c49cae2a0580bb2bbd22aaa05d93615cda30c854
alex <alex_is_ok@yahoo.com>
2019-04-26 22:00:37 +0300

    users controller added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_22_210152) do
+ActiveRecord::Schema.define(version: 2019_04_26_185638) do
 
   create_table "galleries", force: :cascade do |t|
     t.text "title"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 2019_04_22_210152) do
   create_table "massage_types", force: :cascade do |t|
     t.text "title"
     t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "phone"
+    t.string "mail"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
