4dd4bc5d7249f625009cddb88a274571fbfc653e
alex <alex_is_ok@yahoo.com>
2019-04-26 22:49:44 +0300

    modal form fixed for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,10 +28,10 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.html { redirect_back(fallback_location: root_path) }
         format.json { render :show, status: :created, location: @user }
       else
-        format.html { render :new }
+        format.html { redirect_back(fallback_location: root_path) }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
